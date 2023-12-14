4598aa0d5189991dc8130ad19d11d69003a50eea
proX3L <p.reznikov22@gmail.com>
2023-12-14 18:15:16 +0700

    +

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,14 @@
 package koschei.models;
 
 public class Deth8 {
+    private Needle7 needle;
+
+    public Deth8(Needle7 needle) {
+        this.needle = needle;
+    }
+
+    @Override
+    public String toString() {
+        return "смерть Кощея на игле :(";
+    }
 }
--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -4,9 +4,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    private Egg6 egg;
 
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
